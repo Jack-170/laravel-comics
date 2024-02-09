06ef8503be3b66981e7f57ae975c2ed01169f3ce
Jack-170 <agavejack@hotmail>
2024-02-09 18:04:36 +0100

    add cards

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,8 +1,25 @@
 <main>
     <div class="jumbo">
 
-
-
-
     </div>
+
+    <div class="container-fluid bg-dark ">
+        <div class="ms-container">
+            <div class="card-box bg-dark ">
+                @foreach($comics as $comic)
+                    <div class="card bg-dark text-light">
+                        <div class="box-img">
+                            <img src="{{($comic['thumb']) }}" alt="Comic Thumbnail">
+                        </div>
+                        <p>{{ $comic['series'] }}</p>
+                    </div>
+                @endforeach
+            </div>
+
+            <h4 class="text-light">LOAD MORE</h4>
+
+        </div>
+    </div>
+
+
 </main>
